f72432a4e83aaff39b0a914d70d506dcfec02feb
JF Morin <jfmorincamo@gmail.com>
2025-07-21 00:06:20 +0200

    Made the Workspace widget work (almost)

--- dotfiles/ags/.config/ags/widget/Workspaces.tsx
+++ dotfiles/ags/.config/ags/widget/Workspaces.tsx
@@ -1,12 +1,9 @@
 import { Astal, Gtk, Gdk } from "ags/gtk4"
-import { For, Accessor } from "ags"
+import { For, Accessor, createState } from "ags"
 import Hyprland from "gi://AstalHyprland"
 
 export default function Workspaces() {
     const hyprland = Hyprland.get_default()
-    let sortedWorkspaces = new Accessor<Hyprland.Workspace>()
-
-    //hyprland.connect('workspace-added',)
 
     let getSortedWorkspaces = function () {
         let workspaces = hyprland.get_workspaces().sort ((a,b) => {
@@ -14,11 +11,28 @@ export default function Workspaces() {
             if(a.id > b.id) return 1  
             return 0
         })
+        return workspaces
     }
 
-    return (
-        <box orientation={Gtk.Orientation.HORIZONTAL}>
+    const [workspaces, setWorkspaces] = createState(getSortedWorkspaces())
 
+    hyprland.connect('workspace-added', () => {
+        let ws = getSortedWorkspaces()
+        setWorkspaces(ws)
+    })
+    hyprland.connect('workspace-removed', () => { 
+        let ws = getSortedWorkspaces()
+        setWorkspaces(ws)
+    })
+
+
+    return (
+        <box class="bar-widget" orientation={Gtk.Orientation.HORIZONTAL}>
+            <For each={workspaces}>
+                {(item, index: Accessor<number>) => (
+                <label class="workspace-button" label={item.name} />
+                )}
+            </For>
         </box>
     )
 }
